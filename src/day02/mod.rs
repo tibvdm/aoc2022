6d7117ab59142b44cafe3700b972320aa77ee83b
tibvdm <tibovandemoortele@hotmail.com>
2022-12-07 21:27:23 +0100

    Day 2: include_bytes gives a 80% improvement over inlcude_string

--- src/day02/mod.rs
+++ src/day02/mod.rs
@@ -1,34 +1,34 @@
 pub fn part1() -> i32 {
-    include_str!("input.txt")
-        .lines()
+    include_bytes!("input.txt")
+        .split(|&b| b == b'\n')
         .map(|line| match line {
-            "A Y" => 8,
-            "B Z" => 9,
-            "C X" => 7,
-            "A X" => 4,
-            "B Y" => 5,
-            "C Z" => 6,
-            "A Z" => 3,
-            "B X" => 1,
-            "C Y" => 2,
+            b"A Y" => 8,
+            b"B Z" => 9,
+            b"C X" => 7,
+            b"A X" => 4,
+            b"B Y" => 5,
+            b"C Z" => 6,
+            b"A Z" => 3,
+            b"B X" => 1,
+            b"C Y" => 2,
             _     => panic!("Invalid input")
         })
         .sum()
 }
 
 pub fn part2() -> i32 {
-    include_str!("input.txt")
-        .lines()
+    include_bytes!("input.txt")
+        .split(|&b| b == b'\n')
         .map(|line| match line {
-            "A X" => 3,
-            "B X" => 1,
-            "C X" => 2,
-            "A Y" => 4,
-            "B Y" => 5,
-            "C Y" => 6,
-            "A Z" => 8,
-            "B Z" => 9,
-            "C Z" => 7,
+            b"A X" => 3,
+            b"B X" => 1,
+            b"C X" => 2,
+            b"A Y" => 4,
+            b"B Y" => 5,
+            b"C Y" => 6,
+            b"A Z" => 8,
+            b"B Z" => 9,
+            b"C Z" => 7,
             _     => panic!("Invalid input")
         })
         .sum()
